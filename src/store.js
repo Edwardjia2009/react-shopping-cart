7f1ede494466a01991cf739a1b3171dd757f854c
Shicheng Jia <edwardjia2009@gmail.com>
2020-11-27 17:46:23 +0800

    add redux cart

--- src/store.js
+++ src/store.js
@@ -1,13 +1,14 @@
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
 import thunk from 'redux-thunk'
+import { cartReducer } from './reducers/cartReducers'
 import { productReducer } from './reducers/productReducers'
 
 const initialState = {}
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(
-  combineReducers({ products: productReducer }),
+  combineReducers({ products: productReducer, cart: cartReducer }),
   initialState,
   composeEnhancer(applyMiddleware(thunk))
-  )
+)
 
 export default store
